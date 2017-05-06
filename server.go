0d40c205f4ff418f9a18d6356e38d94fa5adecc4
Brian McClain <brianmmcclain@gmail.com>
2017-05-06 11:49:52 -0400

    Allow choice of playback to any device

--- server.go
+++ server.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/zmb3/spotify"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 var client *spotify.Client
 var config Config
+
+type PlayCommand struct {
+	TrackURI string `json:"uri"`
+	DeviceID string `json:"device"`
+}
+
+type SpotifyDevice struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+}
 
 func main() {
 
@@ -22,6 +34,10 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/search/{keyword}", SearchHandler)
+	r.HandleFunc("/play", PlayHandler).Methods("GET")
+	r.HandleFunc("/play", PlayTrackHandler).Methods("POST")
+	r.HandleFunc("/pause", PauseHandler)
+	r.HandleFunc("/devices", DevicesHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
 
@@ -38,5 +54,44 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 
 	var resultJson = fmt.Sprintf("{\"name\": \"%s\", \"artist\": \"%s\", \"URI\": \"%s\"}", track.SimpleTrack.Name, track.SimpleTrack.Artists[0].Name, track.URI)
 	fmt.Println(resultJson)
+
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(resultJson))
+}
+
+func PlayHandler(w http.ResponseWriter, r *http.Request) {
+	client.Play()
+}
+
+func PlayTrackHandler(w http.ResponseWriter, r *http.Request) {
+	b, _ := ioutil.ReadAll(r.Body)
+	var command PlayCommand
+	json.Unmarshal(b, &command)
+
+	pOpts := spotify.PlayOptions{URIs: []spotify.URI{spotify.URI(command.TrackURI)}}
+	if len(command.DeviceID) > 0 {
+		dID := spotify.ID(command.DeviceID)
+		pOpts.DeviceID = &dID
+	}
+	_ = client.PlayOpt(&pOpts)
+}
+
+func PauseHandler(w http.ResponseWriter, r *http.Request) {
+	client.Pause()
+}
+
+func DevicesHandler(w http.ResponseWriter, r *http.Request) {
+	devices, _ := client.PlayerDevices()
+
+	// Build list of devices
+	var sDevices []SpotifyDevice
+	for _, d := range devices {
+		sd := SpotifyDevice{Name: d.Name, ID: string(d.ID)}
+		sDevices = append(sDevices, sd)
+	}
+
+	jDevices, _ := json.Marshal(sDevices)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(jDevices))
 }
